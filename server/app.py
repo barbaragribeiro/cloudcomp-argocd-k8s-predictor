bd332e51e1052f65f18622d6c4e51dcf66a1aa39
Bárbara Gomes <barbaragomes@dcc.ufmg.br>
2022-06-18 02:45:49 +0000

    Add version attribute

--- server/app.py
+++ server/app.py
@@ -1,10 +1,17 @@
+from _version import __version__
 from flask import Flask
 from flask import request
 from flask.json import jsonify
 import pickle
+import time
+import os
+
+model_file = "ml-model/model.pickle"
 
 app = Flask(__name__)
-app._clf_pipeline = pickle.load(open("ml-model/model.pickle", "rb"))
+app._clf_pipeline = pickle.load(open(model_file, "rb"))
+app._version = __version__
+app._model_date = time.ctime(os.path.getmtime(model_file))
 
 @app.route("/api/american", methods=["POST"])
 def predict():
@@ -12,7 +19,12 @@ def predict():
     if not isinstance(text, list):
         text = [text]
     predicted = app._clf_pipeline.predict(text)
-    return jsonify(predicted=predicted.tolist())
+
+    return jsonify(
+        is_american=predicted.tolist(),
+        version=app._version,
+        model_date=app._model_date
+    )
 
 if __name__ == "__main__":
     port = 5005
